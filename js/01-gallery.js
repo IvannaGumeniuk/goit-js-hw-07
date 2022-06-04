90945aafb782f0dff3cf0e29283b42fe2765fe88
IvannaGumeniuk <vany93ok@gmail.com>
2022-06-04 14:55:12 +0300

    додає модальне вікно

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,17 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-// console.log(galleryItems);
-
-// 1 Створення і рендер розмітки на підставі масиву даних galleryItems
-//  і наданого шаблону елемента галереї.
-
-// 2 Реалізація делегування на div.gallery 
-// і отримання url великого зображення.
 
 const galleryContainer = document.querySelector('.gallery');
-const cardsPicture = createPictureCards(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', cardsPicture);
+const imagesList = createImagesCards(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imagesList);
 galleryContainer.addEventListener('click', onPictureComntainerClick);
 
-// document.body.addEventListener('click', onPictureComntainerClick);
 
-function createPictureCards(picture) {
-    return picture.map(({preview, original, description }) => {
+function createImagesCards(picture) {
+    return picture.map(({ preview, original, description }) => {
+        
         return `
         <div class="gallery__item">
-            <a class="gallery__link" >
+            <a class="gallery__link" href="${original}">
                 <img
                 class="gallery__image"
                 src="${preview}"
@@ -33,12 +24,30 @@ function createPictureCards(picture) {
     }).join('');
 }
 
-
 function onPictureComntainerClick(event) {
-    console.log(event.target);
+    event.preventDefault();
+    // console.log(event.target);
+
     const imgEl = event.target.nodeName;
-    if (!event.target.nodeName === 'IMG') {
+    if (!imgEl === 'IMG') {
         return;
     }
+    
+    const instance = basicLightbox.create(`
+        <img src="${event.target.closest('img').dataset.source}" width="800" height="600">
+    `);
 
+    instance.show();
 }
+
+
+document.addEventListener("keydown", event => {
+if (!document.querySelector('.basicLightbox')) {
+      return;
+}
+    const basicLightboxEL = document.querySelector('.basicLightbox');
+    if (event.code === 'Escape') {
+        basicLightboxEL.remove();
+    }
+
+});
